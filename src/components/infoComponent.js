9081c5ab80c31e81d942332eef20138a745eb592
ömer harun çetin <omerharuncetin@gmail.com>
2021-01-09 20:25:48 +0300

    Initial commit

---
+++ src/components/infoComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../styles/index.css"
+export const InfoComponent = () => {
+
+    return (
+        <div>
+            Alfabe : 1, 2, 3, 4, 5
+            <br/>
+            <span className="text-muted">Lütfen sadece alfabe üyelerini içeren inputu giriniz.</span>
+        </div>
+    )
+}
---
+++ src/components/numberInput.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Input } from 'antd';
+
+
+class NumericInput extends React.Component {
+    onChange = e => {
+        const { value } = e.target;
+        const reg = /^-?\d*(\.\d*)?$/;
+        if ((!isNaN(value) && reg.test(value)) || value === '' || value === '-') {
+            this.props.onChange(value);
+        }
+    };
+
+    // '.' at the end or only '-' in the input box.
+    onBlur = () => {
+        const { value, onBlur, onChange } = this.props;
+        let valueTemp = value;
+        if (value.charAt(value.length - 1) === '.' || value === '-') {
+            valueTemp = value.slice(0, -1);
+        }
+        onChange(valueTemp.replace(/0*(\d+)/, '$1'));
+        if (onBlur) {
+            onBlur();
+        }
+    };
+
+    render() {
+        return (
+            <Input
+                {...this.props}
+                onChange={this.onChange}
+                onBlur={this.onBlur}
+                placeholder="Input a number"
+                maxLength={25}
+            />
+        );
+    }
+}
+export default NumericInput
